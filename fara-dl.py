11f0235f5f29115c2971cee5da4e7ee74086e1ef
Rashida Kamal <rsk2161@columbia.edu>
2019-12-11 10:05:43 -0500

    more

--- fara-dl.py
+++ fara-dl.py
@@ -3,6 +3,18 @@ import requests
 import json
 import os
 
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+def runServer():
+    app.run()
+
 def downloadRegistrants(startDate, endDate):
 
     endpoint = "https://efile.fara.gov/api/v1/Registrants/json/New?from={}&to={}".format(startDate, endDate)
@@ -26,19 +38,30 @@ def downloadSupplementalForms():
     
     return 
 
+
+
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description="Download PDFs from DOJ's")
     # parser.add_argument("--search", "-s", dest="search_term")
 
+    # potential flags 
+
     parser.add_argument("--supplemental", "-s", dest='supplemental', default=None, required=False, help="Use to download supplement forms for each registrant in registrants.json")
-    parser.add_argument("--dates", "-d", dest='dates', required=False, help="Specify a start date and end date as MM-DD-YYYY, separating each with a space")
+
+    parser.add_argument("--explore", "-e", dest='explore', default=None, required=False, help="Use to download supplement forms for each registrant in registrants.json")
+
+    parser.add_argument("--dates", "-d", dest='dates', required=True, help="Specify a start date and end date as MM-DD-YYYY, separating each with a space")
     args = parser.parse_args()
 
     dates = args.dates.split(" ")
     supplemental = args.supplemental 
+    explore = args.explore
 
     downloadRegistrants(dates[0], dates[1])
 
     if supplemental is not None:
         downloadSupplementalForms()
+
+    if explore is not None:
+        runServer()
